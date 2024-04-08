71040d19d950c8e14370e31fae4362946668df59
Umit Bulut <umitbulut1993@hotmail.com>
2024-04-08 01:57:00 -0400

    User,DTO and Entity Folder has created - 04/08/2024

---
+++ src/main/java/com/cydeo/dto/RoleDTO.java
@@ -0,0 +1,10 @@
+package com.cydeo.dto;
+
+
+
+public class RoleDTO {
+
+    private Long id;
+    private String description;
+
+}
---
+++ src/main/java/com/cydeo/enums/Gender.java
@@ -0,0 +1,17 @@
+package com.cydeo.enums;
+
+import lombok.val;
+
+public enum Gender {
+    MALE("Male"),FEMALE("Female");
+
+    private String value;
+
+    Gender(String value) {
+        this.value=value;
+    }
+    public String getValue(){
+        return value;
+    }
+}
+
